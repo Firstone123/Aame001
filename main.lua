01ababf5cfaf3a8a4430c46777e47607eb4c5df1
edword <edword0710@yahoo.com.tw>
2017-11-14 17:30:07 +0800

    123

--- main.lua
+++ main.lua
@@ -11,6 +11,6 @@ world.y = display.contentCenterY - 30
 <<<<<<< HEAD
 local msgText = display.newText( "edowrd0710", world.x, world.y+160, native.systemFont, 32 )
 =======
-local msgText = display.newText( "Hi, world!", world.x, world.y+160, native.systemFont, 32 )
+local msgText = display.newText( "fghjkl;", world.x, world.y+160, native.systemFont, 32 )
 >>>>>>> be88100e63b28b30137fbd6298e2a8820dabf8c7
 msgText:setFillColor( 0.2, 0.6, 0.8 )
